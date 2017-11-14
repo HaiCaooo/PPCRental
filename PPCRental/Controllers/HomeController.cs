9ae41033ce4725ca12d7ebe03f84ca4f2efa1e29
HaiCao <haicao4@vanlanguni.vn>
2017-11-15 00:45:31 +0700

    Add project files.

---
+++ PPCRental/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCRental.Models;
+namespace PPCRental.Controllers
+{
+    public class HomeController : Controller
+    {
+        DemoPPCRentalEntities m = new DemoPPCRentalEntities();
+        public ActionResult Index()
+        {
+            var p = m.PROPERTies.ToList();
+            return View(p);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Search(string text)
+        {
+            var p = m.PROPERTies.ToList().Where(x => x.PropertyName.Contains(text)
+                || x.ID.ToString().Contains(text) || x.Content.Contains(text));
+            return View(p);
+        }
+    }
+}
---
+++ PPCRental/Controllers/propertyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCRental.Models;
+namespace PPCRental.Controllers
+{
+    public class propertyController : Controller
+    {
+        //
+        // GET: /property/
+        DemoPPCRentalEntities m = new DemoPPCRentalEntities();
+        public ActionResult Detail(int id,string name)
+        {
+            var p = m.PROPERTies.FirstOrDefault(x => x.ID == id || x.PropertyName == name);
+            return View(p);
+        }
+	}
+}
